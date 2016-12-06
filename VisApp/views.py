b1f8fc2a0cc0276670a52e141c6ef919b6639a15
Ubuntu <ubuntu@ip-172-31-19-182.ap-northeast-1.compute.internal>
2016-12-06 09:58:03 +0000

    kmeans implemented

--- VisApp/views.py
+++ VisApp/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render, redirect
 from django.conf import settings
 from django.core.files.storage import FileSystemStorage
+
+from sklearn.cluster import KMeans
+import numpy as np
+
 import json
 import csv
+
 
 # Create your views here.
 def main(request):
@@ -25,8 +30,17 @@ def overview(request):
 
 def kmeans(request):
 
+	with open(settings.STATIC_ROOT + "/test.csv","r") as f:
+		data = list(csv.reader(f, delimiter=','))
+		column_names = data[0]
+		data = [[float(i) for i in row][:-1] for row in data[1:]]
 
-		
+	kmeans = KMeans(n_clusters=6).fit(data)
+
+	for i, row in enumerate(data):
+		row.append(kmeans.labels_[i])
+	print(data)
+
 	return render(request, 'kmeans.html')
 
 def dbscan(request):
