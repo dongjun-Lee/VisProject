36377a913c7a23c4e145e3349bfebec137bf539b
galsang <kimtaeuk1000@naver.com>
2016-12-08 00:54:33 +0900

    views.py dbscan backbone implemented

--- VisApp/views.py
+++ VisApp/views.py
@@ -5,6 +5,7 @@ from django.core.files.storage import FileSystemStorage
 from django.http import HttpResponse
 
 from sklearn.cluster import KMeans
+from sklearn.cluster import DBSCAN
 import numpy as np
 
 import json
@@ -30,46 +31,67 @@ def uploadFile(request):
 def overview(request):
 	return render(request, 'overview.html')
 
-def do_kmeans(K,max_iter,n_init,selected_column_names):
+def preprocess_csv(selected_column_names=["x","y"]):
 	with open(settings.STATIC_ROOT + "/test.csv","r") as f:
 		data = list(csv.DictReader(f))
-
-		selected_dics = []
-		selected_list = []
+		selected_data = []
 		for row in data:
 			selected_row = dict([(key,row[key]) for key in selected_column_names])
-			selected_dics.append(selected_row)
-			selected_list.append(selected_row.values())
+			selected_data.append(selected_row)
 
-	kmeans = KMeans(n_clusters=K,max_iter=max_iter,n_init=n_init).fit(selected_list)
+		return selected_data
 
-	for i, row in enumerate(selected_dics):
-		selected_dics[i]["class"] = str(kmeans.labels_[i])
+def convert4train(data):
+    return [row.values() for row in data]
+
+def do_kmeans(K=2, max_iter=300, n_init=10):
+	data = preprocess_csv()
+
+	kmeans = KMeans(n_clusters=K,max_iter=max_iter,n_init=n_init).fit(convert4train(data))
+	for i, row in enumerate(data):
+	    data[i]["class"] = str(kmeans.labels_[i])
 	
-	return selected_dics
+	return data
+
+def do_dbscan(eps=0.5, min_samples=5):
+	data = preprocess_csv()
+
+	dbscan = DBSCAN(eps=eps, min_samples=min_samples).fit(convert4train(data))
+	for i, row in enumerae(data):
+		data[i]["class"] = str(dbscan.labels_[i])
+
+	return data
 
 def kmeans(request):
-	K = 2
-	max_iter = 300
-	n_init = 10
 	selected_column_names = ["x","y"]
 
-	result = do_kmeans(K,max_iter,n_init,selected_column_names)
+	result = do_kmeans()
 	return render(request, 'kmeans.html', {"data": tuple(result)})
 
 def ajax_kmeans(request):
-	K = int(request.GET["K"].encode('utf-8'))
-	max_iter = int(request.GET["max_iter"].encode('utf-8'))
-	n_init = int(request.GET["n_init"].encode('utf-8'))
+	K = int(request.GET["K"].encode("utf-8"))
+	max_iter = int(request.GET["max_iter"].encode("utf-8"))
+	n_init = int(request.GET["n_init"].encode("utf-8"))
 	selected_column_names = ["x","y"]
 
-	result = do_kmeans(K, max_iter, n_init,selected_column_names)
+	result = do_kmeans(K, max_iter, n_init)
 	mimetype = "application/json"
 	return HttpResponse(json.dumps(result), mimetype)
 
 def dbscan(request):
-	return render(request, 'dbscan.html')
+	selected_column_names = ["x","y"]
 
+	result = do_dbscan()
+	return render(request, 'dbscan.html', {"data": tuple(result)})
+
+def ajax_dbscan(request):
+	eps = int(request.GET["eps"].encode("utf-8"))
+	min_samples = int(request.GET["min_samples"].encode("utf-8"))
+	selected_column_names = ["x","y"]
+
+	result = do_dbscan(eps=eps, min_samples=min_samples)
+	mimetype = "application/json"
+	return HttpResponse(json.dumps(result), mimetype)
 
 def hierarchical(request):
 	return render(request, 'hierarchical.html')
