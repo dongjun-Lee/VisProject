1d33f72a702ad1182723297e2b5d698927311aaf
galsang <kimtaeuk1000@naver.com>
2016-12-08 16:51:08 +0900

    refactoring

--- VisApp/views.py
+++ VisApp/views.py
@@ -32,71 +32,59 @@ def overview(request):
 	return render(request, 'overview.html')
 
 def load_csv(selected_columns):
-	with open(settings.STATIC_ROOT + "/test.csv","r") as f:
+	with open(settings.MEDIA_ROOT + "/data.csv","r") as f:
 		data = list(csv.DictReader(f))
+		if not selected_columns:
+			selected_columns = sorted(list(data[0].keys()))
 		selected_data = []
 		for row in data:
 			selected_row = dict([(key,row[key]) for key in selected_columns])
 			selected_data.append(selected_row)
 
-		return selected_data
+		return selected_data, selected_columns
 
 def conv2array(data):
-    return np.array([row.values() for row in data])
+	return np.array([row.values() for row in data])
 
-def do_kmeans(selected_columns, K=2, max_iter=300, n_init=10):
-	data = load_csv(selected_columns)
+def do_clustering(selected_columns=[],method="kmeans",K=2,max_iter=300,eps=1.5,min_samples=5):
+	data, selected_columns = load_csv(selected_columns)
 	columns = data[0].keys()
 
-	kmeans = KMeans(n_clusters=K, max_iter=max_iter, n_init=n_init).fit(conv2array(data))
+	if method == "kmeans":
+		result = KMeans(n_clusters=K, max_iter=max_iter).fit(conv2array(data))
+	else:
+		result = DBSCAN(eps=eps, min_samples=min_samples).fit(conv2array(data))
+		
 	for i, row in enumerate(data):
-	    data[i]["class"] = str(kmeans.labels_[i])
-	
-	return data, columns
+	    data[i]["class"] = str(result.labels_[i])
 
-def do_dbscan(selected_columns, eps=1.5, min_samples=5):
-	data = load_csv(selected_columns)
+	column_dics = [dict([("name", c), ("selected", c in selected_columns)]) for c in columns]
 
-	dbscan = DBSCAN(eps=eps, min_samples=min_samples).fit(conv2array(data))
-	for i, row in enumerate(data):
-		data[i]["class"] = str(dbscan.labels_[i])
-
-	return data
-
-def make_column_dics(columns, selected_columns):
-	 return [dict[("name", c), ("selected", c in selected_columns)] for c in columns]
-			result.append(dict[("name",c), ("selected",False)])
-	return [dict[("name", c), ("selected", (c in selected_columns))] for c in columns]
+	return data, column_dics
 
 def kmeans(request):
-	selected_columns= ["x","y"]
-	result, columns = do_kmeans(selected_columns)
-		tuple(make_column_dics(columns, seleted_columns))})
-	return render(request, 'kmeans.html', {"data": tuple(result), "columns":
-		tuple(make_column_dics(columns, selected_columns))})
+	result, columns = do_clustering(method="kmeans")
+	return render(request, 'kmeans.html', {"data": tuple(result), "columns": tuple(columns)})
+
+def dbscan(request):
+	result, columns = do_clustering(method="dbscan")
+	return render(request, 'dbscan.html', {"data": tuple(result)})
 
 def ajax_kmeans(request):
 	K = int(request.GET["K"].encode("utf-8"))
 	max_iter = int(request.GET["max_iter"].encode("utf-8"))
-	n_init = int(request.GET["n_init"].encode("utf-8"))
 	selected_columns= ["x","y"]
 
-	result = do_kmeans(selected_columns, K, max_iter, n_init)
+	result, columns = do_clustering(selected_columns=selected_columns, method="kmeans", K=K, max_iter=max_iter)
 	mimetype = "application/json"
 	return HttpResponse(json.dumps(result), mimetype)
-
-def dbscan(request):
-	selected_columns= ["x","y"]
-
-	result = do_dbscan(selected_columns)
-	return render(request, 'dbscan.html', {"data": tuple(result)})
 
 def ajax_dbscan(request):
 	eps = float(request.GET["eps"].encode("utf-8"))
 	min_samples = int(request.GET["min_samples"].encode("utf-8"))
 	selected_columns= ["x","y"]
 
-	result = do_dbscan(selected_columns, eps=eps, min_samples=min_samples)
+	result = do_clustering(seleted_columns=selected_columns, method="dbscan", eps=eps, min_samples=min_samples)
 	mimetype = "application/json"
 	return HttpResponse(json.dumps(result), mimetype)
 
